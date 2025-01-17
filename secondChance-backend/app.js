acae514ded82bc9c7ee0c8d62d4422b914f4a19a
abdellah404 <1234abdellah007@gmail.com>
2025-01-17 05:31:27 +0100

    commit

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -7,7 +7,6 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
-
 const app = express();
 app.use("*",cors());
 const port = 3060;
